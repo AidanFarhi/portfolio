637633eed3ece4779857ed09437b567a647a921f
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-10 13:12:24 -0400

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Sidebar extends React.Component {
+
+    render() {
+        return (
+            <div className='sidebar'>
+                <img>My Face Here</img>
+                <button>Projects</button>
+                <button>Skills</button>
+                <button>About</button>
+                <button>Contact</button>
+            </div>
+        )
+    }
+}
+
+export default Sidebar
