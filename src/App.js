bbd8152727f578359f5e14f9f0638da28013e247
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-10 21:06:04 -0400

    skeleton of site is up. switch page feature working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Sidebar from './components/Sidebar'
+import Projects from './components/Projects'
+import About from './components/About'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
+
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      rendering: 'projects'
+    }
+  }
+
+  switchPage = (str) => {
+    const newPage = str
+    this.setState({
+      rendering: newPage
+    })
+  }
   
   render() {
+    const page = this.state.rendering
+    console.log(page)
     return (
       <div>
-        <Sidebar />
+        <Sidebar onClick={this.switchPage}/>
+        {page === 'projects' ? <Projects /> : null}
+        {page === 'skills' ? <Skills /> : null}
+        {page === 'about' ? <About /> : null}
+        {page === 'contact' ? <Contact /> : null}
       </div>
     )
   }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
 class Sidebar extends React.Component {
-
+    constructor(props) {
+        super(props) 
+            this.state = {
+                onClick: props.onClick
+            }
+        
+    }
     render() {
         return (
             <div className='sidebar'>
-                <img>My Face Here</img>
-                <button>Projects</button>
-                <button>Skills</button>
-                <button>About</button>
-                <button>Contact</button>
+                <div>My Face Here</div>
+                <ul>
+                    <li><button onClick={()=> this.state.onClick('projects')}>Projects</button></li>
+                    <li><button onClick={()=> this.state.onClick('skills')}>Skills</button></li>
+                    <li><button onClick={()=> this.state.onClick('about')}>About</button></li>
+                    <li><button onClick={()=> this.state.onClick('contact')}>Contact</button></li>
+                </ul>
             </div>
         )
     }
