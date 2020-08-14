9a412533f9e02f0cddd489ef8bfed3fb5a470ac5
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-14 14:01:09 -0400

    created Skills.css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './cmp-styles/Skills.css'
 
 export default function Skills() {
     return (
-        <div className='main'>
+        <div className='skills-main-div'>
             These are my Skills
         </div>
     )
