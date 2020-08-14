da3d0f38bf61133230b516699440560b973f89ab
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-14 14:06:42 -0400

    cleaned up App.js, added About.js and Contact.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,9 @@ import React from 'react'
 import Routes from './Routes'
 import NavBar from './NavBar'
 
-const styles = {
-  main: {
-    display: 'flex',
-    flexDirection: 'row'
-  }
-}
-
 function App() {
   return (
-    <div style={styles.main}>
-    {/* Header */}
+    <div>
     <NavBar />
     <Routes />
     </div>
