1cf22156b01d43532ad7b45106210b1512c5ea03
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-19 09:44:35 -0400

    added state to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import face from './images/portfolio.jpg'
 import './styles/NavBar.css'
 
 
 function NavBar() {
+
+    const [state, setState] = useState({
+        currentPage: ''
+    })
+
+    const handleClick = (page) => {
+        setState({ currentPage: page })
+        console.log(state)
+    }
+
     return(
         <div className='nav-bar'>
             <img id='face' src={face} alt='Aidan'/>
             <p id='name'>Aidan Farhi</p>
             <p id='title'>Full-Stack Web Developer</p>
-            <Link className='link' to='/projects'>Projects</Link>
-            <Link className= 'link' to='/resume'>Resume</Link>
-            <Link className='link' to='/about'>About</Link>
-            <Link className= 'link' to='/contact'>Contact</Link>
+            <Link onClick={()=> handleClick('projects')} className='link' to='/projects'>Projects</Link>
+            <Link onClick={()=> handleClick('resume')} className='link' to='/resume'>Resume</Link>
+            <Link onClick={()=> handleClick('about')} className='link' to='/about'>About</Link>
+            <Link onClick={()=> handleClick('contact')} className='link' to='/contact'>Contact</Link>
             <p id='footer'>Site powered by <span style={{color: 'lightblue'}}>React</span></p>
         </div>
     )
