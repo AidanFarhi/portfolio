3eb80415f4d6519a86b748c362a3c7fea25d5a71
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-20 09:51:11 -0400

    added feature where link wil stay highlighted depending on page. Bug where if you refresh state gets wiped. changed profile pic and background

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import face from './images/portfolio.jpg'
+import face2 from './images/aidan-face-2.png'
 import './styles/NavBar.css'
 
 
@@ -14,18 +15,37 @@ function NavBar() {
         setState({ currentPage: page })
     }
 
-    // add global style object here
-    
+    // useEffect(()=> {
+    //     this.setState({
+
+    //     })
+    // })
 
     return(
         <div className='nav-bar'>
-            <img id='face' src={face} alt='Aidan'/>
+            <img id='face' src={face2} alt='Aidan'/>
             <p id='name'>Aidan Farhi</p>
             <p id='title'>Full-Stack Web Developer</p>
-            <Link onClick={()=> handleClick('projects')} className='link' to='/projects'>Projects</Link>
-            <Link onClick={()=> handleClick('resume')} className='link' to='/resume'>Resume</Link>
-            <Link onClick={()=> handleClick('about')} className='link' to='/about'>About</Link>
-            {/* <Link onClick={()=> handleClick('contact')} className='link' to='/contact'>Contact</Link> */}
+            <Link onClick={()=> handleClick('projects')} 
+                className={state.currentPage === 'projects' ? 'link selected' : 'link'}
+                to='/projects'>
+                Projects
+            </Link>
+            <Link onClick={()=> handleClick('resume')} 
+                className={state.currentPage === 'resume' ? 'link selected' : 'link'} 
+                to='/resume'>
+                Resume
+            </Link>
+            <Link onClick={()=> handleClick('about')} 
+                className={state.currentPage === 'about' ? 'link selected' : 'link'} 
+                to='/about'>
+                About
+            </Link>
+            {/* <Link onClick={()=> handleClick('contact')} 
+                    className='link' 
+                    to='/contact'>
+                    Contact
+                </Link> */}
             <p id='footer'>Site powered by <span style={{color: 'lightblue'}}>React</span></p>
         </div>
     )
