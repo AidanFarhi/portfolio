2d967d2282790ba671c7d0d30de03582bdb1d42e
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-27 16:35:01 -0400

    mobile responsive

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import face3 from './images/aidan-face-3.png'
+import menu from './images/menu.png'
 import './styles/NavBar.css'
 
 
@@ -22,30 +23,37 @@ function NavBar() {
 
     return(
         <div className='nav-bar'>
+            <img id='menu' src={menu} alt='menu'/>
+            <input type='checkbox' />
             <img id='face' src={face3} alt='Aidan'/>
             <p id='name'>Aidan Farhi</p>
             <p id='title'>Full-Stack Web Developer</p>
-            <Link onClick={()=> handleClick('projects')} 
-                className={state.currentPage === 'projects' ? 'link selected' : 'link'}
-                to='/projects'>
-                Projects
-            </Link>
-            <Link onClick={()=> handleClick('resume')} 
-                className={state.currentPage === 'resume' ? 'link selected' : 'link'} 
-                to='/resume'
-                id='bottom-link'>
-                Resume
-            </Link>
-            <Link onClick={()=> handleClick('about')} 
-                className={state.currentPage === 'about' ? 'link selected' : 'link'} 
-                to='/about'>
-                About
-            </Link>
-            <div id='contact-div'>
-                <h3 id='contact-header'>Contact</h3>
-                <p><a href='mailto:aidanmcfarhi@gmail.com' target='_blank' rel="noopener noreferrer">aidanmcfarhi@gmail.com</a></p>
-                <p>718-419-0795</p>
+            <div id='toggle'>
+                <div id='links'>
+                    <Link onClick={()=> handleClick('projects')} 
+                        className={state.currentPage === 'projects' ? 'link selected' : 'link'}
+                        to='/projects'>
+                        Projects
+                    </Link>
+                    <Link onClick={()=> handleClick('resume')} 
+                        className={state.currentPage === 'resume' ? 'link selected' : 'link'} 
+                        to='/resume'
+                        id='bottom-link'>
+                        Resume
+                    </Link>
+                    <Link onClick={()=> handleClick('about')} 
+                        className={state.currentPage === 'about' ? 'link selected' : 'link'} 
+                        to='/about'>
+                        About
+                    </Link>
+                </div>  
+                <div id='contact-div'>
+                    <h3 id='contact-header'>Contact</h3>
+                    <p><a href='mailto:aidanmcfarhi@gmail.com' target='_blank' rel="noopener noreferrer">aidanmcfarhi@gmail.com</a></p>
+                    <p>718-419-0795</p>
+                </div>
             </div>
+            
         </div>
     )
 }
