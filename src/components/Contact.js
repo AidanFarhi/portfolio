2a4244671a4b45dfebb723c21363153a911f31ed
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-30 10:06:16 -0400

    mobile responsiveness better

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './cmp-styles/Contact.css'
+
+export default function Contact() {
+    return (
+        <div id='contact-main-div'>
+            <h1 id='contact-header'>Contact</h1>
+            <div id='contact-div'>
+                <h3><a href='mailto:aidanmcfarhi@gmail.com' target='_blank' rel="noopener noreferrer">aidanmcfarhi@gmail.com</a></h3>
+                <h3>646-500-1595</h3>
+            </div>
+        </div>
+    )
+}
