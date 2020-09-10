a0b1d7b88baee8bd8402cea958b4aa1b3b09f7e3
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-09-10 09:36:09 -0400

    fixed typo

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
             <div id='about-me-div'>
                 <p>
                     "I am a software engineer that labors to build applications that
-                    are not only efficient, pracical and useful, but simple and intutive for the user.
+                    are not only efficient, practical and useful, but simple and intutive for the user.
                     I am a firm beliver in life-long learning, and am always willing and able to
                     absorb new technologies and methods, whenever possible."
                 </p>
