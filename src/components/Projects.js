7ed39d2747773904d7e38a08f479546915f97a95
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-04-02 22:50:46 -0400

    added amazon product analyzer

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './cmp-styles/Projects.css'
 
-import leetRides from './project-images/leet-rides-new.png'
 import sudoku from './project-images/sudoku.png'
-import linktree from './project-images/linktree.png'
+import amazon from './project-images/amazon-analyzer.png'
 import reactIcon from './icons/react.jpeg'
 import nodeIcon from './icons/node.png' 
-import postG from './icons/postgresIcon.png'
-import expressIcon from './icons/express.jpeg'
+import pythonIcon from './icons/python.png'
+import flaskIcon from './icons/flask.png'
+// import postG from './icons/postgresIcon.png'
+// import expressIcon from './icons/express.jpeg'
 import js from './icons/js.png'
-import cloudflare from './icons/cloudflare.png'
 
 export default function Projects() {
     return (
@@ -17,27 +17,19 @@ export default function Projects() {
             <h1 id='project-header'>Projects</h1>
             <div className='project-item'>
                 <div className='description'>
-                    <h3>LeetRides</h3>
-                    <p>This is a full-stack e-commerce application that supports both registered
-                        users and guests.
+                    <h3>Amazon Product Analyzer</h3>
+                    <p>
+                        This is a web application that takes in an amazon product url and
+                        calculates a score.
                     </p>
                 </div>
-                <a href='https://leetrides.herokuapp.com/' target='_blank' rel="noopener noreferrer">
-                    <img className='project-image'alt='test' src={leetRides}/>
+                <a href='https://github.com/AidanFarhi/Product-Sentiment/' target='_blank' rel="noopener noreferrer">
+                    <img className='project-image'alt='test' src={amazon}/>
                 </a>
                 <div className='icons'>
                     <img alt='icon' src={reactIcon}/>
-                    <img alt='icon' src={nodeIcon}/>
-                    <img alt='icon' src={expressIcon}/>
-                    <img alt='icon' src={postG}/>
-                </div>
-                <div className='description'>
-                    <h3>Features</h3>
-                    <ul id='features'>
-                        <li>Stripe API for credit card transactions</li>
-                        <li>Inventory Search</li>
-                        <li>Guest checkout</li>
-                    </ul>
+                    <img alt='icon' src={pythonIcon}/>
+                    <img alt='icon' src={flaskIcon}/>
                 </div>
             </div>
             <div className='project-item'>
@@ -59,27 +51,6 @@ export default function Projects() {
                     <ul id='features'>
                         <li>Multiple game difficulties</li>
                         <li>Minimialist style. No hints or number counts.</li>
-                    </ul>
-                </div>
-            </div>
-            <div className='project-item'>
-                <div className='description'>
-                    <h3>Linktree Site</h3>
-                    <p>A linktree style micro-profile site.
-                    </p>
-                </div>
-                <a href='https://assignment.afarhidev.workers.dev/' target='_blank' rel="noopener noreferrer">
-                    <img className='project-image'alt='test' src={linktree}/>
-                </a>
-                <div className='icons'>
-                    <img alt='icon' src={js}/>
-                    <img alt='icon' src={cloudflare}/>
-                </div>
-                <div className='description'>
-                    <h3>Features</h3>
-                    <ul id='features'>
-                        <li>Links to sites made by me.</li>
-                        <li>Clickable icons to personal social media.</li>
                     </ul>
                 </div>
             </div>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,9 +8,10 @@ export default function Resume() {
             <div id='resume'>
                 <h2>Profile</h2>
                 <p>
-                I am a full-stack web developer that strives to build efficient applications that are both intuitive and user-friendly. 
+                I am a software engineer that strives to build efficient applications that are both intuitive and user-friendly. 
                 I thrive in a team-oriented environment and fully embrace all the joys and challenges of building software. 
-                I love diving deep into all areas of software engineering, and am always looking to learn new things and add to my skills.
+                I love diving deep into all areas of software engineering, and am looking to break into the feild as a full-stack engineer or
+                a data engineer.
                 </p>
                 <a href='https://github.com/AidanFarhi' target='_blank' rel="noopener noreferrer">Link to my github</a>
 
@@ -18,9 +19,9 @@ export default function Resume() {
                 <h4>Front-end</h4>
                 <p>React | JavaScript | HTML | CSS </p>
                 <h4>Back-end</h4>
-                <p>Node | Express | PostgreSQL | Python</p>
-                <h4>Mobile</h4>
-                <p>React-Native</p>
+                <p>Node | Express | PostgreSQL | Flask</p>
+                <h4>Programming Languages</h4>
+                <p>Python | JavaScript | Java | C++ | C</p>
 
                 <h2>Education</h2>
                 <h4>CUNY TechWorks Summer Bootcamp (remote) - June 2020 - September 2020</h4>
@@ -28,14 +29,8 @@ export default function Resume() {
                     PostgreSQL, Express, Node and React-Native as well as gaining algorithm solving expertise.
                 </p>
 
-                {/* <h4>IT Support Professional (Google) - July 2020 - present</h4>
-                <p>- Training to become an IT Support Professional while learning the basics of computer assembly, 
-                    OS installation, internet protocol, computer networking, 
-                    Linux and Windows, cybersecurity and system administration.
-                </p> */}
-
-                <h4>Algorithms and Data Structures in Python (Udemy) - September 2020 - present</h4>
-                <p>- Learning and implementing essential data structures including, but not limited
+                <h4>Algorithms and Data Structures in Python (Udemy Certification) - September 2020 - December 2020</h4>
+                <p>- Learned and implemented essential data structures including, but not limited
                     to; linked lists, trees, heaps, graphs, stacks, queues, as well as the accompanying searching, 
                     sorting, path finding, and other algorithms in Python.
                 </p>
@@ -46,6 +41,10 @@ export default function Resume() {
                 </p>
                 
                 <h2>Experience</h2>
+                <h4>Personal Projects - July 2020 - present</h4>
+                <p>- Built several programs and projects ranging from: web-scraping, data pipelines, web applications,
+                    image filters, and others.
+                </p>
                 <h4>CUNY TechWorks (remote) July 2020 - September 2020</h4>
                 <p>- Led a team in building a full-stack e-commerce application 
                     using GitHub for version control and Trello for task management.
